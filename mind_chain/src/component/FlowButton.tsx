eaa8ec4de838cc69f33bfc38aa397967a9e8f8d2
iLikePuffs2 <iLikePuffs2@gmail.com>
2024-04-17 16:16:38 +0800

    完成新增节点的样式(还差data)

--- mind_chain/src/component/FlowButton.tsx
+++ mind_chain/src/component/FlowButton.tsx
@@ -1,10 +1,6 @@
 // FlowButton.tsx
 import React from "react";
-import ReactFlow, {
-  Controls,
-  Background,
-  Panel,
-} from "reactflow";
+import ReactFlow, { Controls, Background, Panel } from "reactflow";
 import "reactflow/dist/style.css";
 import { LayoutAlgorithm } from "../utils/LayoutAlgorithm";
 import useCopyPaste from "../utils/useCopyPaste";
@@ -20,7 +16,6 @@ function FlowButton({
   onConnect,
   onLayout,
 }) {
-
   const { cut, copy, paste, bufferedNodes } = useCopyPaste();
   const canCopy = nodes.some(({ selected }) => selected);
   const canPaste = bufferedNodes.length > 0;
@@ -29,6 +24,37 @@ function FlowButton({
     const layouted = LayoutAlgorithm(nodes, edges);
     setNodes([...layouted.nodes]);
     setEdges([...layouted.edges]);
+  };
+
+  // 新增节点
+  const addNode = () => {
+    const newNodeId = `node-${Date.now()}`;
+    let newNodePosition = {
+      x: rootNode.position.x + 200,
+      y: rootNode.position.y,
+    };
+
+    // 循环判断是否有重合节点,直到没有重合节点为止
+    let overlappingNode;
+    do {
+      overlappingNode = nodes.find(
+        (node) =>
+          node.position.x === newNodePosition.x &&
+          node.position.y === newNodePosition.y
+      );
+      if (overlappingNode) {
+        // 如果有重合节点,就上移一些
+        newNodePosition.y -= 50;
+      }
+    } while (overlappingNode);
+
+    const newNode = {
+      id: newNodeId,
+      data: { label: "新节点" },
+      position: newNodePosition,
+    };
+
+    setNodes([...nodes, newNode]);
   };
 
   return (
@@ -45,6 +71,7 @@ function FlowButton({
         <Controls />
         <Panel position="top-right">
           <button onClick={handleLayout}>重新排布</button>
+          <button onClick={addNode}>新增节点</button>
         </Panel>
       </ReactFlow>
     </div>
