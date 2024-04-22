191157b4113c0ef75eca4eb0d2d81bc35d824b95
iLikePuffs2 <iLikePuffs2@gmail.com>
2024-04-22 19:53:59 +0800

    修复状态流转的bug

--- mind_chain/src/utils/ConvertStatus/BlockNode.ts
+++ mind_chain/src/utils/ConvertStatus/BlockNode.ts
@@ -9,10 +9,13 @@ export const blockNode = (currentNode, nodes, setNodes, edges, blockType) => {
     // 修改节点的status为2
     currentNodeObj.data.status = 2;
 
-    // 根据传入的标识,在details的末尾添加数字
+    // 根据传入的标识,在details中添加或覆盖数字
     const details = currentNodeObj.data.details;
     if (details) {
-      if (details.includes(blockType)) {
+      if (details === "1" || details === "2") {
+        // 如果details的值为1或2,就直接覆盖
+        currentNodeObj.data.details = blockType;
+      } else if (details.includes(blockType)) {
         // 如果已有要加的数字,就不改动
       } else {
         // 如果没有要加的数字,就添加
--- mind_chain/src/utils/ConvertStatus/CalculateStatus.ts
+++ mind_chain/src/utils/ConvertStatus/CalculateStatus.ts
@@ -7,66 +7,68 @@
 export function calculateNodeStatusAndDetails(nodes: Node[], edges: Edge[]) {
   const updatedNodes = nodes.map((node) => ({ ...node })); // 创建节点的副本
 
+  // 第一次遍历：所有收敛节点及其子节点
+  // a.先找到所有从上到下的收敛节点
+  const convergenceNodes: Node[] = [];
+  updatedNodes.forEach((node) => {
+    if (node.id === "0") return; // 排除根节点
+    const convergenceNode = findConvergenceNodeBelow(node, updatedNodes, edges);
+    if (convergenceNode !== null) {
+      convergenceNodes.push(convergenceNode);
+    }
+  });
+
+  // b.然后找出以它们作为起始节点,向下的所有路径里经过的所有节点。再将所有收敛节点和这些节点的 status 设为2,details添加5
+  const nodesUnderConvergenceNodes = new Set<string>();
+
+  convergenceNodes.forEach((convergenceNode) => {
+    const nodesSet = findNodesUnderConvergenceNode(
+      convergenceNode,
+      updatedNodes,
+      edges
+    );
+    nodesSet.forEach((nodeId) => {
+      nodesUnderConvergenceNodes.add(nodeId);
+    });
+  });
+
+  updatedNodes.forEach((node) => {
+    if (
+      convergenceNodes.some(
+        (convergenceNode) => convergenceNode.id === node.id
+      ) ||
+      nodesUnderConvergenceNodes.has(node.id)
+    ) {
+      node.data.status = 2;
+      node.data.details = node.data.details ? node.data.details + ",5" : "5";
+    }
+  });
+
+  // c.最后,移除所有其他节点的 details 里的5
+  updatedNodes.forEach((node) => {
+    if (
+      !convergenceNodes.some(
+        (convergenceNode) => convergenceNode.id === node.id
+      ) &&
+      !nodesUnderConvergenceNodes.has(node.id) &&
+      node.id !== "0"
+    ) {
+      node.data.details = node.data.details.replace(",5", "").replace("5", "");
+    }
+  });
+
+  // 第二次遍历:全部的直接子节点被阻塞
   // 找到所有最底层的节点(不作为任何线段source的节点)
   const bottomNodes = updatedNodes.filter(
     (node) => !edges.some((edge) => edge.source === node.id)
   );
 
-  // 第一次遍历：上方存在同级收敛节点
-  for (let i = updatedNodes.length - 1; i >= 0; i--) {
-    const node = updatedNodes[i];
-    if (node.id === "0") continue; // 跳过根节点
-
-    const convergenceNode = findConvergenceNodeAbove(node, updatedNodes, edges); // 找到当前节点上方的收敛节点
-    if (convergenceNode) {
-      node.data.status = 2; // 将状态设为2
-      node.data.details = node.data.details.replace("1", "").replace("2", ""); // 剔除详情1和2
-      node.data.details = node.data.details ? node.data.details + ",5" : "5"; // 添加详情5，考虑逗号
-    } else {
-      node.data.details = node.data.details.replace(",5", "").replace("5", ""); // 移除详情5，考虑逗号
-    }
-  }
-
-  // 第二次遍历:唯一的直接父节点被阻塞
   bottomNodes.forEach((bottomNode) => {
-    bfs(bottomNode, updatedNodes, edges, (node) => {
-      if (node.id === "0") return; // 跳过根节点
-
-      const parentNodes = getDirectParentNodes(node.id, updatedNodes, edges); // 获取直接父节点
-      if (parentNodes.length === 1 && parentNodes[0].data.status === 2) {
-        node.data.status = 2; // 将状态设为2
-        node.data.details = node.data.details.replace("1", "").replace("2", ""); // 剔除详情1和2
-        node.data.details = node.data.details ? node.data.details + ",6" : "6"; // 添加详情6,考虑逗号
-      } else {
-        node.data.details = node.data.details
-          .replace(",6", "")
-          .replace("6", ""); // 移除详情6,考虑逗号
-      }
-    });
+    const paths = findPathsAbove(bottomNode.id, updatedNodes, edges);
+    updateNodeStatusAndDetails(paths, updatedNodes, edges);
   });
 
-  // 第三次遍历:全部的直接子节点被阻塞
-  bottomNodes.forEach((bottomNode) => {
-    bfs(bottomNode, updatedNodes, edges, (node) => {
-      if (node.id === "0") return; // 跳过根节点
-
-      const childNodes = getDirectChildNodes(node.id, updatedNodes, edges); // 获取直接子节点
-      if (
-        childNodes.length > 0 &&
-        childNodes.every((childNode) => childNode.data.status === 2)
-      ) {
-        node.data.status = 2; // 将状态设为2
-        node.data.details = node.data.details.replace("1", "").replace("2", ""); // 剔除详情1和2
-        node.data.details = node.data.details ? node.data.details + ",7" : "7"; // 添加详情7,考虑逗号
-      } else {
-        node.data.details = node.data.details
-          .replace(",7", "")
-          .replace("7", ""); // 移除详情7,考虑逗号
-      }
-    });
-  });
-
-  // 第四次遍历:可直接执行、有任意子节点可执行
+  // 第三次遍历:可直接执行、有任意子节点可执行
   for (let i = updatedNodes.length - 1; i >= 0; i--) {
     const node = updatedNodes[i];
     if (node.id === "0") break; // 跳过根节点
@@ -180,43 +182,198 @@ function getDirectChildNodes(
 }
 
 /**
- * 找到当前节点上方的收敛节点
+ * 找到当前节点下方的收敛节点
  *
  * @param node 当前节点
  * @param nodes 节点列表
  * @param edges 边列表
- * @returns 上方的收敛节点，如果没有则返回null
+ * @returns 下方的收敛节点,如果没有则返回null
  */
-function findConvergenceNodeAbove(
+function findConvergenceNodeBelow(
   node: Node,
   nodes: Node[],
   edges: Edge[]
 ): Node | null {
-  const visited = new Set<string>(); // 记录已访问的节点ID
-  let convergenceNode: Node | null = null; // 收敛节点
+  // 找出以当前节点为起点的所有边
+  const outgoingEdges = edges.filter((edge) => edge.source === node.id);
 
-  const dfs = (nodeId: string, level: number) => {
-    visited.add(nodeId); // 将当前节点标记为已访问
+  // 如果只有一条边,且目标节点的level与当前节点相同,则该节点为收敛节点
+  if (
+    outgoingEdges.length === 1 &&
+    nodes.find((n) => n.id === outgoingEdges[0].target).data.level ===
+      node.data.level
+  ) {
+    return nodes.find((n) => n.id === outgoingEdges[0].target);
+  }
 
-    const parentEdges = edges.filter((edge) => edge.target === nodeId); // 获取当前节点的父节点边
+  // 找出当前节点向下的所有路径
+  const paths: Node[][] = [];
+  outgoingEdges.forEach((edge) => {
+    const path = findPathsBelow(edge.target, nodes, edges);
+    paths.push(...path);
+  });
 
-    for (const parentEdge of parentEdges) {
-      const parentNodeId = parentEdge.source;
-      const parentNode = nodes.find((n) => n.id === parentNodeId);
+  // 找出所有路径第一次交汇的节点
+  const convergenceNode = findFirstCommonNode(paths);
 
-      if (parentNode && !visited.has(parentNodeId)) {
-        if (parentNode.data.level == level) {
-          convergenceNode = parentNode; // 找到收敛节点
-          return;
-        }
-        dfs(parentNodeId, level); // 递归遍历父节点
-        if (convergenceNode) {
-          return;
-        }
+  // 如果交汇节点的level与当前节点相同,则该节点为收敛节点
+  if (convergenceNode && convergenceNode.data.level === node.data.level) {
+    return convergenceNode;
+  }
+
+  return null;
+}
+
+/**
+ * 找出以指定节点为起点向下的所有路径
+ *
+ * @param nodeId 起始节点ID
+ * @param nodes 节点列表
+ * @param edges 边列表
+ * @returns 所有向下的路径
+ */
+function findPathsBelow(
+  nodeId: string,
+  nodes: Node[],
+  edges: Edge[]
+): Node[][] {
+  const paths: Node[][] = [];
+
+  // 找出以指定节点为起点的所有边
+  const outgoingEdges = edges.filter((edge) => edge.source === nodeId);
+
+  if (outgoingEdges.length === 0) {
+    // 如果没有出边,说明已经到达叶子节点,将当前节点作为一条路径返回
+    paths.push([nodes.find((n) => n.id === nodeId)]);
+  } else {
+    // 递归查找每条出边的目标节点向下的路径
+    outgoingEdges.forEach((edge) => {
+      const subPaths = findPathsBelow(edge.target, nodes, edges);
+      subPaths.forEach((subPath) => {
+        paths.push([nodes.find((n) => n.id === nodeId), ...subPath]);
+      });
+    });
+  }
+
+  return paths;
+}
+
+/**
+ * 找出以指定节点为起点向上的所有路径
+ *
+ * @param nodeId 起始节点ID
+ * @param nodes 节点列表
+ * @param edges 边列表
+ * @returns 所有向上的路径
+ */
+function findPathsAbove(
+  nodeId: string,
+  nodes: Node[],
+  edges: Edge[]
+): Node[][] {
+  const paths: Node[][] = [];
+  // 找出以指定节点为终点的所有边
+  const incomingEdges = edges.filter((edge) => edge.target === nodeId);
+
+  if (incomingEdges.length === 0) {
+    // 如果没有入边,说明已经到达根节点,将当前节点作为一条路径返回
+    paths.push([nodes.find((n) => n.id === nodeId)]);
+  } else {
+    // 递归查找每条入边的源节点向上的路径
+    incomingEdges.forEach((edge) => {
+      const subPaths = findPathsAbove(edge.source, nodes, edges);
+      subPaths.forEach((subPath) => {
+        // 反转 subPath 数组(保证从叶子节点指向根节点)
+        const reversedSubPath = [...subPath].reverse();
+        paths.push([...reversedSubPath, nodes.find((n) => n.id === nodeId)]);
+      });
+    });
+  }
+
+  return paths;
+}
+
+/**
+ * 找到以指定收敛节点为起点向下的所有路径中经过的所有节点
+ * @param convergenceNode 指定收敛节点
+ * @param nodes 节点列表
+ * @param edges 边列表
+ * @returns
+ */
+function findNodesUnderConvergenceNode(
+  convergenceNode: Node,
+  nodes: Node[],
+  edges: Edge[]
+): Set<string> {
+  const paths = findPathsBelow(convergenceNode.id, nodes, edges);
+  const nodesSet = new Set<string>();
+
+  paths.forEach((path) => {
+    path.forEach((node) => {
+      nodesSet.add(node.id);
+    });
+  });
+
+  return nodesSet;
+}
+
+/**
+ * 找出多条路径第一次交汇的节点
+ *
+ * @param paths 路径列表
+ * @returns 第一次交汇的节点,如果没有则返回null
+ */
+function findFirstCommonNode(paths: Node[][]): Node | null {
+  if (paths.length === 0) {
+    return null;
+  }
+
+  const visitedNodes = new Map<string, number>();
+
+  for (const path of paths) {
+    for (const node of path) {
+      const nodeId = node.id;
+      visitedNodes.set(nodeId, (visitedNodes.get(nodeId) || 0) + 1);
+      if (visitedNodes.get(nodeId) === paths.length) {
+        return node;
       }
     }
-  };
+  }
 
-  dfs(node.id, node.data.level); // 从当前节点开始深度优先搜索
-  return convergenceNode;
+  return null;
+}
+
+// 遍历路径和节点,更新节点的状态和详情
+function updateNodeStatusAndDetails(
+  paths: Node[][],
+  updatedNodes: Node[],
+  edges: Edge[]
+) {
+  paths.forEach((path) => {
+    path.forEach((node) => {
+      if (node.id === "0") return; // 跳过根节点
+
+      // 找到当前节点的直接子节点
+      const childNodes = getDirectChildNodes(node.id, updatedNodes, edges);
+      // 如果全部直接子节点状态都为被阻塞，就把当前节点也设为被阻塞，details里补上7
+      if (
+        childNodes.length > 0 &&
+        childNodes.every((childNode) => childNode.data.status === 2)
+      ) {
+        node.data.status = 2;
+        node.data.details = node.data.details.replace("1", "").replace("2", "");
+        node.data.details = node.data.details ? node.data.details + ",7" : "7";
+      } else {
+        // 反之，移除details里的7
+        node.data.details = node.data.details
+          .replace(",7", "")
+          .replace("7", "");
+
+        // 如果在移除 7 之后 details 没有值了,就将 node.data.status 设为 1
+        if (node.data.details.trim() === "") {
+          node.data.status = 1;
+        }
+      }
+    });
+  });
 }
