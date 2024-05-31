bfcda619ae05dc0055a75c0209b10bdac31f3596
iLikePuffs2 <iLikePuffs2@gmail.com>
2024-06-01 00:40:53 +0800

    完成保存笔记和重新排布的快捷键

--- mind_chain/src/pages/Flow.tsx
+++ mind_chain/src/pages/Flow.tsx
@@ -151,6 +151,42 @@ const Flow = () => {
     };
   }, [nodes, setNodes]);
 
+  // 使用快捷键进行界面重排和保存
+const handleKeyDown = useCallback(
+  (event) => {
+    if (event.ctrlKey) {
+      event.preventDefault();
+      switch (event.key) {
+        // ctrl+s 保存笔记
+        case "s":
+          saveAndUpdateNote(
+            userId,
+            selectedNote?.id || "",
+            selectedNote?.name || "",
+            nodes
+          );
+          break;
+        // ctrl+q 重新排布
+        case "q":
+          handleLayout(nodes, edges);
+          break;
+        default:
+          break;
+      }
+    }
+  },
+  [userId, selectedNote, nodes, edges]
+);
+
+// 根据快捷键的变化,触发不同事件
+useEffect(() => {
+  window.addEventListener("keydown", handleKeyDown);
+
+  return () => {
+    window.removeEventListener("keydown", handleKeyDown);
+  };
+}, [handleKeyDown]);
+
   // 重新排布
   const handleLayout = (nodes, edges) => {
     const layouted = LayoutAlgorithm(nodes, edges);
